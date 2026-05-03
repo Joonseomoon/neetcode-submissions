2aef0b22eb814e7d25c23702711aa5d8cdba78a3
Joonseo Moon <jsmoon416@gmail.com>
2026-05-03 11:56:08 -0400

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        
+        countRef = collections.defaultdict(int)
+        countWin = collections.defaultdict(int)
+
+        for c in t:
+            countRef[c] += 1
+        
+        have = 0
+        need = len(countRef)
+        l = 0
+        minLen = float("inf")
+        res = ""
+
+
+        for r in range(l, len(s)):
+            if s[r] in countRef:
+                countWin[s[r]] += 1 
+                if countWin[s[r]] == countRef[s[r]]:
+                    have += 1
+
+            
+            while have == need:
+                if (r - l + 1) < minLen:
+                    minLen = r - l + 1
+                    res = s[l:r + 1]
+
+                
+                if s[l] in countRef:
+                    countWin[s[l]] -= 1
+                    if countWin[s[l]] < countRef[s[l]]:
+                        have -= 1
+                l += 1
+
+        return res
+            
