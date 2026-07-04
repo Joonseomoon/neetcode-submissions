5006886a404d1dca3ba357e5c9130cf57a5137b9
Joonseo Moon <jsmoon416@gmail.com>
2026-07-04 11:06:54 -1000

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        copyMap = collections.defaultdict()
+
+        q = collections.deque()
+        res = Node(node.val)
+        q.append((node, res))
+        copyMap[node] = res
+
+
+        while q:
+            curr, copy = q.popleft()
+            for neighbor in curr.neighbors:
+                if neighbor not in copyMap:
+                    temp = Node(neighbor.val)
+                    q.append((neighbor, temp))
+                    copyMap[neighbor] = temp
+                copy.neighbors.append(copyMap[neighbor])
+        return res
